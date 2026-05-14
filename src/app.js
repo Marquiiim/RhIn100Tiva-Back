37bea867339835ef3d7db04c3fe99e8952d1b112
Marcos Gabriel <marcosindev@gmail.com>
2026-05-13 22:32:37 -0300

    new config cors

--- src/app.js
+++ src/app.js
@@ -6,15 +6,24 @@ const rateLimiter = require('./middlewares/rateLimiter')
 const app = express()
 
 const allowedOrigins = [
-    //PÚBLICO
-    'https://rh-in100-tiva-front.vercel.app?_vercel_share=iYHMw8vxfkm3pn6XjvzDEMovcw1VTkti',
+    // Frontend público (Vercel)
+    'https://rh-in100-tiva-front.vercel.app',
 
-    //PRIVADO
-    'https://rh-in100-tiva-front-j0joo3s5z-marquiiims-projects.vercel.app'
+    // Frontend privado/de preview (Vercel)
+    'https://rh-in100-tiva-front-j0joo3s5z-marquiiims-projects.vercel.app',
 ]
 
 app.use(cors({
-    origin: 'https://rh-in100-tiva-front.vercel.app?_vercel_share=iYHMw8vxfkm3pn6XjvzDEMovcw1VTkti'
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true)
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            console.warn(`CORS bloqueado para: ${origin}`);
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
 }))
 
 app.use(express.json())
